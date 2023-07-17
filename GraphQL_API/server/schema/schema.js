0abef057400055f24e685784459d63b29e0dffc4
virtualblakewillis <bewillis101@gmail.com>
2023-07-17 16:02:10 -0600

    Task 2

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,6 +1,13 @@
 // schema property 
 const graphql = require('graphql');
 const { GraphQLString, GraphQLInt, GraphQLSchema, GraphQLObjectType } = graphql;
+const _ = require('lodash');
+
+// Task 2, dummy data for resolve function
+let tasks = [
+  {id: '1', title: 'Create your first webpage', weight: 1, description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'},
+  {id: '2', title: 'Structure your webpage', weight: 1, description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order'},
+];
 
 // Task 0, create TaskType schema
 let TaskType = new GraphQLObjectType({
@@ -23,12 +30,13 @@ let RootQuery = new GraphQLObjectType({
         id: { type: GraphQLString }
       },
       resolve(parent, args) {
-        // code to get data from db / other source
+        // Task 2, use id to find task using lodash and return it
+        return _.find(tasks, {id: args.id});
       }
     }
   }
 });
 
-module.export = new GraphQLSchema({
+module.exports = new GraphQLSchema({
   query: RootQuery
 });

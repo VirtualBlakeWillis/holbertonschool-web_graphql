ef1a5792dc946673df6f49171d66f014dc88b054
virtualblakewillis <bewillis101@gmail.com>
2023-07-17 15:44:36 -0600

    task 1 - works/! had to fix my schema definitions with GraphQLObjectType

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,8 +1,9 @@
 // schema property 
 const graphql = require('graphql');
-const { GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const { GraphQLString, GraphQLInt, GraphQLSchema, GraphQLObjectType } = graphql;
 
-let TaskType = new GraphQLSchema({
+// Task 0, create TaskType schema
+let TaskType = new GraphQLObjectType({
   name: 'Task',
   fields: () => ({
     id: { type: GraphQLString },
@@ -10,4 +11,24 @@ let TaskType = new GraphQLSchema({
     weight: { type: GraphQLInt },
     description: { type: GraphQLString },
   })
+});
+
+// Task 1, create RootQuery schema
+let RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    task: {
+      type: TaskType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parent, args) {
+        // code to get data from db / other source
+      }
+    }
+  }
+});
+
+module.export = new GraphQLSchema({
+  query: RootQuery
 });

82032b285233594667073e50f783c567824c0151
virtualblakewillis <bewillis101@gmail.com>
2023-07-17 16:25:38 -0600

    Task 3 done

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -14,10 +14,26 @@ let tasks = [
   {id: '1', title: 'Create your first webpage', weight: 1, description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'},
   {id: '2', title: 'Structure your webpage', weight: 1, description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order'},
 ];
+// Task 3, dummy data for resolve function
+let projects = [
+  {id: "1", title: "Advanced HTML", weight: 1, description: "Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don’t worry, the final page will be “ugly” it’s normal, it’s not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!"},
+  {id: "2", title: "Bootstrap", weight: 1, description: "Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components."},
+];
 
 // Task 0, create TaskType schema
 let TaskType = new GraphQLObjectType({
   name: 'Task',
+  fields: () => ({
+    id: { type: GraphQLID },
+    title: { type: GraphQLString },
+    weight: { type: GraphQLInt },
+    description: { type: GraphQLString },
+  })
+});
+
+// Task 3, create ProjectType schema
+let ProjectType = new GraphQLObjectType({
+  name: 'Project',
   fields: () => ({
     id: { type: GraphQLID },
     title: { type: GraphQLString },
@@ -38,6 +54,16 @@ let RootQuery = new GraphQLObjectType({
       resolve(parent, args) {
         // Task 2, use id to find task using lodash and return it
         return _.find(tasks, {id: args.id});
+      }
+    },
+    // Task 3, create project query
+    project: {
+      type: ProjectType,
+      args: {
+        id: { type: GraphQLID }
+      },
+      resolve(parent, args) {
+        return _.find(projects, {id: args.id});
       }
     }
   }

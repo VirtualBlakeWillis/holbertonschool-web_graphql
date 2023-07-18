f627036c77131a35d53e5452e0b2d5013cff6309
virtualblakewillis <bewillis101@gmail.com>
2023-07-18 15:27:10 -0600

    Task 6. done, but not testing, becuase im not sure i can

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,10 +1,11 @@
 // Initalize Express server with express-graphql as middleware
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
-
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+const url = 'mongodb+srv://admin:admin@cluster0.6ivkslo.mongodb.net/?retryWrites=true&w=majority';
 
 app.use('/graphql',graphqlHTTP({
   schema: schema,
@@ -12,4 +13,12 @@ app.use('/graphql',graphqlHTTP({
 }));
 app.listen(4000,()=>{
   console.log('now listening for request on port 4000');
+});
+
+
+// Task 6, connecting to MongoDB
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connection.once('open', () => {
+console.log('connected to database');
 });

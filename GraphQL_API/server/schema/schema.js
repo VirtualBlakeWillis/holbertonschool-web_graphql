beebd7eafb908aadf90120ec05c34aa892d63d68
virtualblakewillis <bewillis101@gmail.com>
2023-07-19 09:34:59 -0600

    Task 8 done

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -14,15 +14,15 @@ const Project = require('../models/project');
 const _ = require('lodash');
 
 // Task 2, dummy data for resolve function
-let tasks = [
-  {id: '1', projectId: '1', title: 'Create your first webpage', weight: 1, description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'},
-  {id: '2', projectId: '1', title: 'Structure your webpage', weight: 1, description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order'},
-];
-// Task 3, dummy data for resolve function
-let projects = [
-  {id: "1", title: "Advanced HTML", weight: 1, description: "Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don’t worry, the final page will be “ugly” it’s normal, it’s not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!"},
-  {id: "2", title: "Bootstrap", weight: 1, description: "Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components."},
-];
+// let tasks = [
+//   {id: '1', projectId: '1', title: 'Create your first webpage', weight: 1, description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'},
+//   {id: '2', projectId: '1', title: 'Structure your webpage', weight: 1, description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order'},
+// ];
+// // Task 3, dummy data for resolve function
+// let projects = [
+//   {id: "1", title: "Advanced HTML", weight: 1, description: "Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don’t worry, the final page will be “ugly” it’s normal, it’s not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!"},
+//   {id: "2", title: "Bootstrap", weight: 1, description: "Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components."},
+// ];
 
 // Task 0, create TaskType schema
 let TaskType = new GraphQLObjectType({
@@ -35,7 +35,8 @@ let TaskType = new GraphQLObjectType({
     // Task 4, create project query
     project: { type: ProjectType, 
       resolve(parent, args) {
-        return _.find(projects, {id: parent.projectId});
+        // Task 8, update resolve to use mongodb Models
+        return Project.find().where({id: parent.projectId});
       },
     }
   })
@@ -52,7 +53,7 @@ let ProjectType = new GraphQLObjectType({
     // Task 4, create tasks query
     tasks: { type: new GraphQLList(TaskType),
       resolve(parent, args) {
-        return _.filter(tasks, {projectId: parent.id});
+        return Task.find().where({projectId: parent.id});
       },
     }
   })
@@ -69,7 +70,7 @@ let RootQuery = new GraphQLObjectType({
       },
       resolve(parent, args) {
         // Task 2, use id to find task using lodash and return it
-        return _.find(tasks, {id: args.id});
+        return Task.findById(args.id);
       }
     },
     // Task 3, create project query
@@ -79,20 +80,20 @@ let RootQuery = new GraphQLObjectType({
         id: { type: GraphQLID }
       },
       resolve(parent, args) {
-        return _.find(projects, {id: args.id});
+        return Project.findById(args.id);
       }
     },
     // Task 5, tasks & projects query
     tasks: {
       type: new GraphQLList(TaskType),
       resolve(parent, args) {
-        return tasks;
+        return Task.find();
       }
     },
     projects: {
       type: new GraphQLList(ProjectType),
       resolve(parent, args) {
-        return projects;
+        return Project.find();
       }
     },
   })
